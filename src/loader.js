9a14cb78b4effb131b070aac8473e23c03708d99
crlimacastro <crlimacastro@gmail.com>
2021-04-30 00:42:07 -0400

    Added boilerplate files

---
+++ src/loader.js
@@ -0,0 +1,7 @@
+import * as main from './main.js';
+
+window.onload = () => {
+    // Preload - fonts, images, sounds, etc...
+
+    main.init();
+};
---
+++ src/main.js
@@ -0,0 +1,7 @@
+const init = () => {
+
+};
+
+export {
+    init
+};
